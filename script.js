fb1af76516913a4e1ac1e289475f2986d6944a8f
Shivam Gupta <shivamrvgupta@gmail.com>
2022-09-24 16:23:36 +0530

    our team part

---
+++ script.js
@@ -0,0 +1,42 @@
+//for our team section
+
+const btns = document.querySelectorAll('.btn');
+const storeProducts = document.querySelectorAll('.store-product');
+
+for (i = 0; i < btns.length; i++) {
+
+    btns[i].addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const filter = e.target.dataset.filter;
+
+
+        storeProducts.forEach((product) => {
+            if (product.classList.contains(filter)) {
+                product.style.display = 'flex'
+            } else {
+                product.style.display = 'none'
+            }
+
+        });
+    });
+};
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product-details h2");
+
+// A BETTER WAY TO FILTER THROUGH THE PRODUCTS
+search.addEventListener("keyup", filterProducts);
+
+function filterProducts(e) {
+    const text = e.target.value.toLowerCase();
+    productName.forEach(function (product) {
+        const item = product.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            product.parentElement.parentElement.style.display = "block"
+        } else {
+            product.parentElement.parentElement.style.display = "none"
+        }
+    })
+}
